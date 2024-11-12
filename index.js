241a58e158301fcbcc39154e0729de7590954b5d
syarif-hsb <hidayatullah.hsb@gmail.com>
2024-11-12 18:28:17 +0100

    feat: Initial Rolodex page with empty content

---
+++ index.js
@@ -0,0 +1,34 @@
+const contacts = [
+  {
+    name: "John Doe",
+    email: "john@doe.com",
+    phone: "123-456-7890",
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "USA",
+      zip: "12345",
+      something: {
+        somethingElse: "something else",
+      },
+    },
+    group: "family",
+  },
+  {
+    name: "Jane Smith",
+    email: "jane@smith.com",
+    phone: "098-765-4321",
+    address: {
+      street: "456 Elm St",
+      city: "Anytown",
+      state: "USA",
+      zip: "12345",
+      something: {
+        somethingElse: "something else",
+      },
+    },
+    group: "friends",
+  },
+];
+
+console.log(contacts);
