0ec834fb482ddcd432ed3b5401b6a48bc626c8ac
syarif-hsb <hidayatullah.hsb@gmail.com>
2024-11-23 23:11:55 +0100

    fix: Add the js code to render contact element into contact page

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,23 @@
 function renderContact() {
-  const url = new URL(window.location.href)
-  const id = url.searchParams.get("id") 
+  const url = new URL(window.location.href);
+  const id = url.searchParams.get("id");
 
-  
+  const contactToRender = contactsData.filter((contact) => {
+    return contact.id === id;
+  })[0];
+
+  const elementToRender = `
+<div class="p-8 max-w-4xl">
+  <h1 id="full-name" class="text-3xl pb-1">${contactToRender.name}</h1>
+  <span id="work-info" class="pb-1"
+    >${contactToRender.workInfo.jobTitle} | ${contactToRender.workInfo.department} | ${contactToRender.workInfo.company}</span
+  >
+  <hr class="my-2" />
+</div>
+  `;
+
+  const contactContainerElement = document.getElementById("contact-content");
+  contactContainerElement.innerHTML = elementToRender;
 }
 
 renderContact();
