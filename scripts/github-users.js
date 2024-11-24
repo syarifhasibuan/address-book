f20f5a3dc2c964ac1cc320d6086dafcabb96bd13
M Haidar Hanif <git@mhaidarhanif.com>
2024-11-25 01:19:17 +0700

    prepare github-users experiment

---
+++ scripts/github-users.js
@@ -0,0 +1,8 @@
+fetch("https://api.github.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching GitHub users:", error);
+  });
